e685863a21555e2308ec9973db7a4217d9bb9697
etkalecc <etkalecc@m.ffzg.hr>
2026-03-21 14:31:28 +0100

    Create doktori.py

---
+++ doktori.py
@@ -0,0 +1,14 @@
+DOKTORI = {
+    "Ivica Lukšić":"https://najdoktor.com/Ivica-Luk%C5%A1i%C4%87/d10194",
+    "Zvonimir Oršanić":"https://najdoktor.com/zvonimir-orsanic/d18618",
+    "Aleks Finderle":"https://najdoktor.com/aleks-finderle/d12654",
+    "Boris Božić":"https://najdoktor.com/boris-bozic/d9913",
+    "Stipe Ćorluka":"https://najdoktor.com/dr-stipe-corluka/d18436",
+    "Davor Franičević":"https://najdoktor.com/davor-franicevic/d16194",
+    "Zoran Lajtman":"https://najdoktor.com/zoran-lajtman/d8426",
+    "Bojan Šain":"https://najdoktor.com/bojan-sain-drmeddent/d22124",
+    "Miroslav Vukić":"https://najdoktor.com/miroslav-vukic/d8931",
+    "Ana Knap-Vojković":"https://najdoktor.com/ana-knap-vojkovic/d16452",
+    "Vinko Bakula":"https://najdoktor.com/Vinko-Bakula/d16889",
+    "Maja Jagodić-Štimac":"https://najdoktor.com/Maja-Jagodi%C4%87-%C5%A0timac,%20dr.med.dent/d10646"
+}
